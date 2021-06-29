fb2f23e0205803c988e124cfba6aaf80c366f54b
Shrimietewary127 <64962246+Shrimietewary127@users.noreply.github.com>
2021-06-29 19:01:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,43 +8,56 @@ function setup() {
 
 function draw() {
   background(255,255,255);  
+  translate(200,200)
+  rotate(-90)
   hr = hour();
   min=minute();
   sc=second();
   angleMode(DEGREES);
   
-  scAngle=map(sc-15,0,60,0,360)
-  hrAngle=map(hr-3,0,12,0,360)
-  minAngle=map(min-15,0,60,0,360)
+  scAngle=map(sc,0,60,0,360)
+  hrAngle=map(hr % 12,0,12,0,360)
+  minAngle=map(min,0,60,0,360)
 
   noFill()
+  push()
   strokeWeight(10)
   stroke('red')
-  arc(200,200,380,380,990,scAngle)
+  arc(0,0,380,380,990,scAngle)
+  pop()
 
   noFill()
   strokeWeight(10)
   stroke('blue')
-  arc(200,200,320,320,990,hrAngle)
+  arc(0,0,320,320,990,hrAngle)
 
   noFill()
   strokeWeight(10)
   stroke('green')
-  arc(200,200,350,350,990,minAngle)
+  arc(0,0,350,350,990,minAngle)
 
+  push()
   stroke(255,0,0);
   strokeWeight(7);
-  line(200,200,100,100,scAngle)
+  rotate(scAngle)
+  line(0,0,100,0)
+  pop()
 
+  push()
   stroke('green');
   strokeWeight(7);
-  line(200,200,200,105,minAngle)
+  rotate(minAngle)
+  line(0,0,75,0)
+  pop()
 
+  push()
   stroke('blue');
   strokeWeight(7);
-  line(200,200,300,160,hrAngle)
-  
+  rotate(hrAngle)
+  line(0,0,50,0)
+  pop()
   
   
   drawSprites();
 }
+
